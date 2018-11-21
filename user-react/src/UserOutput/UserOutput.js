2dd73f1dae785ca7e7645a719183c22220d1da00
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-11-20 20:09:20 -0500

    Added components and input field and 2 paragraphs

---
+++ user-react/src/UserOutput/UserOutput.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const userOutput = () => {
+  return (
+    <div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos sit dicta
+        nulla, laboriosam distinctio id?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
+        voluptates cupiditate, itaque vitae maxime quaerat!
+      </p>
+    </div>
+  );
+};
+
+export default userOutput;
